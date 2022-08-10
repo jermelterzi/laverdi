3951661d7fcf8f93e9efac40da86e11f11e43258
jermelterzi <jv_ermel@hotmail.com>
2022-08-09 23:04:23 -0300

    Implementing app with routes, fonts, theme color and a splash page

---
+++ lib/pages/splash_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+import 'auth_page.dart';
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const AuthPage();
+  }
+}
---
+++ lib/utils/app_routes.dart
@@ -0,0 +1,5 @@
+class AppRoutes {
+  static const SPLASH_PAGE = '/';
+  static const HOME_PAGE = '/home-page';
+  static const AUTH_PAGE = '/auth-page';
+}
