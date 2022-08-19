92007aed9bbc363de9b7e83e3f6b72680a2eac49
jermelterzi <jv_ermel@hotmail.com>
2022-08-19 00:24:43 -0300

    Implementing planner widgets, adding two more days to calendar and showing the calendar on click of the header button

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:intl/intl.dart';
+import 'package:laverdi/utils/extensions/string_extension.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  bool _showCalendar = false;
 
   @override
   Widget build(BuildContext context) {
@@ -24,76 +32,110 @@ class HomePage extends StatelessWidget {
 
   Widget _buildBody(BuildContext context) {
     return SafeArea(
-      child: Padding(
-        padding: const EdgeInsets.only(
-          top: 16.0,
-          left: 8.0,
-          right: 8.0,
+      child: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/laverdi_background.jpg'),
+            fit: BoxFit.cover,
+          ),
         ),
-        child: Column(
-          children: [
-            _buildHeader(),
-            _buildCalendar(),
-            _buildPlanner(context),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.only(
+            top: 16.0,
+            left: 8.0,
+            right: 8.0,
+          ),
+          child: Column(
+            children: [
+              _buildHeader(),
+              _showCalendar ? _buildCalendar() : Container(height: 0),
+              _buildPlanner(context),
+            ],
+          ),
         ),
       ),
     );
   }
 
   Widget _buildHeader() {
-    return Row(
-      children: [
-        const CircleAvatar(
-          radius: 32,
-        ),
-        Expanded(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              CircleAvatar(
-                radius: 24,
-                child: Text(
-                  DateTime.now().day.toString().padLeft(2, '0'),
-                  style: const TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-              const SizedBox(width: 8.0),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Text(
-                    'HOJE',
-                    style: TextStyle(
-                      fontSize: 12,
-                      color: Colors.black54,
+    return Padding(
+      padding: const EdgeInsets.only(
+        bottom: 16.0,
+        left: 16.0,
+        right: 16.0,
+      ),
+      child: Row(
+        children: [
+          Expanded(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                CircleAvatar(
+                  radius: 20,
+                  backgroundColor: Theme.of(context).colorScheme.primary,
+                  child: Padding(
+                    padding: const EdgeInsets.only(right: 4.0),
+                    child: Text(
+                      DateTime.now().day.toString().padLeft(2, '0'),
+                      style: const TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
                     ),
                   ),
-                  Text(
-                    DateFormat('EEEE').format(DateTime.now()),
-                    style: const TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ],
-              ),
-              const SizedBox(width: 8.0),
-              CircleAvatar(
-                radius: 14,
-                child: IconButton(
-                  onPressed: () {},
-                  icon: const Icon(FontAwesomeIcons.chevronUp),
-                  iconSize: 12,
                 ),
-              ),
-            ],
+                const SizedBox(width: 8.0),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    _showCalendar
+                        ? const Text(
+                            'HOJE',
+                            style: TextStyle(
+                              fontSize: 12,
+                              color: Colors.black54,
+                            ),
+                          )
+                        : Container(height: 0),
+                    Text(
+                      DateFormat('EEEE', 'pt_BR')
+                          .format(DateTime.now())
+                          .capitalize(),
+                      style: const TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(width: 8.0),
+                CircleAvatar(
+                  radius: 14,
+                  backgroundColor: Theme.of(context).colorScheme.primary,
+                  child: IconButton(
+                    onPressed: () {
+                      setState(() {
+                        _showCalendar = !_showCalendar;
+                      });
+                    },
+                    icon: Icon(
+                      _showCalendar
+                          ? FontAwesomeIcons.chevronUp
+                          : FontAwesomeIcons.chevronDown,
+                      color: Colors.white,
+                    ),
+                    iconSize: 12,
+                  ),
+                ),
+              ],
+            ),
           ),
-        ),
-      ],
+          const CircleAvatar(
+            radius: 28,
+          ),
+        ],
+      ),
     );
   }
 
@@ -101,14 +143,14 @@ class HomePage extends StatelessWidget {
     final DateTime date = DateTime.now();
 
     return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 16.0),
+      padding: const EdgeInsets.only(bottom: 16.0),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
           Column(
             children: [
               Text(
-                  '${DateFormat('EEEE').format(DateTime(date.year, date.month, date.day - 2)).substring(0, 3)}.'),
+                  '${DateFormat('EEEE', 'pt_BR').format(DateTime(date.year, date.month, date.day - 3)).substring(0, 3).capitalize()}.'),
               const SizedBox(height: 8),
               Text(DateTime(date.year, date.month, date.day - 2)
                   .day
@@ -119,7 +161,18 @@ class HomePage extends StatelessWidget {
           Column(
             children: [
               Text(
-                  '${DateFormat('EEEE').format(DateTime(date.year, date.month, date.day - 1)).substring(0, 3)}.'),
+                  '${DateFormat('EEEE', 'pt_BR').format(DateTime(date.year, date.month, date.day - 2)).substring(0, 3).capitalize()}.'),
+              const SizedBox(height: 8),
+              Text(DateTime(date.year, date.month, date.day - 2)
+                  .day
+                  .toString()
+                  .padLeft(2, '0'))
+            ],
+          ),
+          Column(
+            children: [
+              Text(
+                  '${DateFormat('EEEE', 'pt_BR').format(DateTime(date.year, date.month, date.day - 1)).substring(0, 3).capitalize()}.'),
               const SizedBox(height: 8),
               Text(DateTime(date.year, date.month, date.day - 1)
                   .day
@@ -129,7 +182,8 @@ class HomePage extends StatelessWidget {
           ),
           Column(
             children: [
-              Text('${DateFormat('EEEE').format(date).substring(0, 3)}.'),
+              Text(
+                  '${DateFormat('EEEE', 'pt_BR').format(date).substring(0, 3).capitalize()}.'),
               const SizedBox(height: 8),
               Text(date.day.toString().padLeft(2, '0')),
             ],
@@ -137,7 +191,7 @@ class HomePage extends StatelessWidget {
           Column(
             children: [
               Text(
-                  '${DateFormat('EEEE').format(DateTime(date.year, date.month, date.day + 1)).substring(0, 3)}.'),
+                  '${DateFormat('EEEE', 'pt_BR').format(DateTime(date.year, date.month, date.day + 1)).substring(0, 3).capitalize()}.'),
               const SizedBox(height: 8),
               Text(DateTime(date.year, date.month, date.day + 1)
                   .day
@@ -148,9 +202,20 @@ class HomePage extends StatelessWidget {
           Column(
             children: [
               Text(
-                  '${DateFormat('EEEE').format(DateTime(date.year, date.month, date.day + 2)).substring(0, 3)}.'),
+                  '${DateFormat('EEEE', 'pt_BR').format(DateTime(date.year, date.month, date.day + 2)).substring(0, 3).capitalize()}.'),
               const SizedBox(height: 8),
               Text(DateTime(date.year, date.month, date.day + 2)
+                  .day
+                  .toString()
+                  .padLeft(2, '0'))
+            ],
+          ),
+          Column(
+            children: [
+              Text(
+                  '${DateFormat('EEEE', 'pt_BR').format(DateTime(date.year, date.month, date.day + 3)).substring(0, 3).capitalize()}.'),
+              const SizedBox(height: 8),
+              Text(DateTime(date.year, date.month, date.day - 2)
                   .day
                   .toString()
                   .padLeft(2, '0'))
@@ -165,13 +230,152 @@ class HomePage extends StatelessWidget {
     return Expanded(
       child: Container(
         width: double.infinity,
+        padding: const EdgeInsets.all(16),
         decoration: BoxDecoration(
           borderRadius: const BorderRadius.vertical(top: Radius.circular(24.0)),
-          color: Theme.of(context).colorScheme.primary.withOpacity(0.5),
+          color: Theme.of(context).colorScheme.primary.withOpacity(0.3),
         ),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const [Text('PLANNER')],
+          children: [
+            Text(
+              'PLANNER',
+              style: TextStyle(
+                color: Theme.of(context).colorScheme.secondary,
+                fontWeight: FontWeight.bold,
+                fontSize: 15,
+              ),
+            ),
+            const SizedBox(height: 16),
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Container(
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(4.0),
+                      color: Theme.of(context).colorScheme.secondary,
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const [
+                        Icon(
+                          FontAwesomeIcons.cloudSun,
+                          size: 16,
+                          color: Colors.white,
+                        ),
+                        SizedBox(width: 8.0),
+                        Text(
+                          'CAFÉ DA MANHÃ',
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                  Flexible(
+                    flex: 3,
+                    child: ListView.builder(
+                      itemBuilder: (ctx, index) => const Card(
+                        color: Colors.white,
+                        child: SizedBox(
+                          height: 80,
+                          child: Center(
+                            child: Text('Card Exemplo de Café da Manhã'),
+                          ),
+                        ),
+                      ),
+                      itemCount: 1,
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                  Container(
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(4.0),
+                      color: Theme.of(context).colorScheme.secondary,
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const [
+                        Icon(
+                          FontAwesomeIcons.solidSun,
+                          size: 16,
+                          color: Colors.white,
+                        ),
+                        SizedBox(width: 8.0),
+                        Text(
+                          'ALMOÇO',
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                  Flexible(
+                    flex: 3,
+                    child: ListView.builder(
+                      itemBuilder: (ctx, index) => const Card(
+                        color: Colors.white,
+                        child: SizedBox(
+                          height: 80,
+                          child: Center(
+                            child: Text('Card Exemplo de Almoço'),
+                          ),
+                        ),
+                      ),
+                      itemCount: 1,
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                  Container(
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(4.0),
+                      color: Theme.of(context).colorScheme.secondary,
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const [
+                        Icon(
+                          FontAwesomeIcons.solidMoon,
+                          size: 16,
+                          color: Colors.white,
+                        ),
+                        SizedBox(width: 8.0),
+                        Text(
+                          'JANTAR',
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                  Flexible(
+                    flex: 3,
+                    child: ListView.builder(
+                      itemBuilder: (ctx, index) => const Card(
+                        color: Colors.white,
+                        child: SizedBox(
+                          height: 80,
+                          child: Center(
+                            child: Text('Card Exemplo de Jantar'),
+                          ),
+                        ),
+                      ),
+                      itemCount: 1,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
@@ -180,9 +384,9 @@ class HomePage extends StatelessWidget {
   Widget _buildBottomNavigationBar(BuildContext context) {
     return BottomAppBar(
       shape: const CircularNotchedRectangle(),
-      color: Theme.of(context).colorScheme.primary,
+      color: Colors.white,
       child: IconTheme(
-        data: IconThemeData(color: Theme.of(context).colorScheme.onPrimary),
+        data: IconThemeData(color: Theme.of(context).colorScheme.secondary),
         child: Padding(
           padding: const EdgeInsets.all(8.0),
           child: Row(
@@ -213,7 +417,7 @@ class HomePage extends StatelessWidget {
               IconButton(
                 onPressed: () {},
                 icon: const Icon(
-                  FontAwesomeIcons.plateWheat,
+                  FontAwesomeIcons.utensils,
                   size: 32,
                 ),
               ),
