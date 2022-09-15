d36de22a59e953c99cccc2c177d69185ef741066
jermelterzi <jv_ermel@hotmail.com>
2022-09-15 14:00:32 -0300

    Creating recipe model

---
+++ lib/src/features/recipes/models/recipe.dart
@@ -0,0 +1,13 @@
+class Recipe {
+  final String name;
+  final int time;
+  final String difficulty;
+  final int yield;
+
+  Recipe({
+    required this.name,
+    required this.time,
+    required this.difficulty,
+    required this.yield,
+  });
+}
