a0fc7a1cc9c12a65f3c5af905fea86f30a9e7101
jermelterzi <jv_ermel@hotmail.com>
2022-09-15 14:01:17 -0300

    Creating a fab to home

---
+++ lib/src/shared/components/circular_fab.dart
@@ -0,0 +1,95 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class CircularFAB extends StatefulWidget {
+  const CircularFAB({super.key});
+
+  @override
+  State<CircularFAB> createState() => _CircularFABState();
+}
+
+class _CircularFABState extends State<CircularFAB>
+    with SingleTickerProviderStateMixin {
+  final double buttonSize = 56.0;
+  late AnimationController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 250),
+    );
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Flow(
+      delegate: FlowMenuDelegate(controller: controller),
+      children: const <IconData>[
+        FontAwesomeIcons.cartShopping,
+        FontAwesomeIcons.utensils,
+        FontAwesomeIcons.book,
+        FontAwesomeIcons.book,
+        FontAwesomeIcons.plus,
+      ].map(buildFAB).toList(),
+    );
+  }
+
+  Widget buildFAB(IconData icon) {
+    return FloatingActionButton(
+      onPressed: () {
+        if (controller.status == AnimationStatus.completed) {
+          controller.reverse();
+        } else {
+          controller.forward();
+        }
+      },
+      elevation: 0,
+      child: Icon(
+        icon,
+        color: Theme.of(context).colorScheme.onPrimary,
+        size: 32,
+      ),
+    );
+  }
+}
+
+class FlowMenuDelegate extends FlowDelegate {
+  final Animation<double> controller;
+
+  FlowMenuDelegate({required this.controller}) : super(repaint: controller);
+
+  @override
+  void paintChildren(FlowPaintingContext context) {
+    final size = context.size;
+    final xStart = (size.width / 2) - 56;
+    final yStart = size.height - 56;
+
+    final n = context.childCount;
+
+    for (int i = 0; i < n; i++) {
+      final isLastItem = i == context.childCount - 1;
+      setValue(value) => isLastItem ? 0.0 : value;
+
+      final radius = 180 * controller.value;
+      final theta = i * pi * 0.5 / (n - 2);
+
+      final x = xStart - setValue(radius * cos(theta));
+      final y = yStart - setValue(radius * sin(theta));
+
+      context.paintChild(i, transform: Matrix4.identity()..translate(x, y, 0));
+    }
+  }
+
+  @override
+  bool shouldRepaint(covariant FlowDelegate oldDelegate) => false;
+}
