59a18b25cedd28f022ee9a15b65f617bac731916
jermelterzi <jv_ermel@hotmail.com>
2022-09-15 13:56:19 -0300

    Implementing provider to injection of dependecy of meals bloc

--- lib/src/laverdi_app.dart
+++ lib/src/laverdi_app.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:laverdi/src/features/home/bloc/home_bloc.dart';
 import 'package:laverdi/src/shared/app_routes.dart';
+import 'package:provider/provider.dart';
 
 import 'features/home/screens/home_page.dart';
 
@@ -10,30 +12,35 @@ class LaverdiApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'LaVerdi',
-      localizationsDelegates: const [
-        GlobalMaterialLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
+    return MultiProvider(
+      providers: [
+        Provider(create: (_) => HomeBloc()),
       ],
-      supportedLocales: const [
-        Locale('pt', ''),
-      ],
-      theme: ThemeData(
-        fontFamily: 'Fahkwang',
-      ).copyWith(
-        colorScheme: ThemeData().colorScheme.copyWith(
-              primary: const Color(0xFF8CA87C),
-              secondary: HSLColor.fromColor(const Color(0xFF414B3B))
-                  .withLightness(0.5)
-                  .toColor(),
-            ),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'LaVerdi',
+        localizationsDelegates: const [
+          GlobalMaterialLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+        ],
+        supportedLocales: const [
+          Locale('pt', ''),
+        ],
+        theme: ThemeData(
+          fontFamily: 'Fahkwang',
+        ).copyWith(
+          colorScheme: ThemeData().colorScheme.copyWith(
+                primary: const Color(0xFF8CA87C),
+                secondary: HSLColor.fromColor(const Color(0xFF414B3B))
+                    .withLightness(0.5)
+                    .toColor(),
+              ),
+        ),
+        routes: {
+          AppRoutes.HOME_PAGE: (ctx) => const HomePage(),
+        },
       ),
-      routes: {
-        AppRoutes.HOME_PAGE: (ctx) => const HomePage(),
-      },
     );
   }
 }
