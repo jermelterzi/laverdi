18a672bf011f31569070b82c7bcf6a788947e5e5
jermelterzi <joao.terzi@seventh.com.br>
2025-01-19 20:54:22 -0300

    LV-0001 - Feat: Implementing the app theme

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:laverdi/src/utils/theme/theme.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(colorScheme: MaterialTheme.lightScheme()),
       title: 'LaVerdi',
     );
   }
