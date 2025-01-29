1408928772b1655aadd10aae68684481d77d5ef2
jermelterzi <joao.terzi@seventh.com.br>
2025-01-29 05:39:20 -0300

    LV-0002 - Test: Creating credentials fixtures to be used in tests

---
+++ test/src/domain/user/dtos/fixtures/credentials_fixtures.dart
@@ -0,0 +1,10 @@
+import 'package:laverdi/src/domain/user/dtos/credentials.dart';
+
+class CredentialsFixtures {
+  CredentialsFixtures();
+
+  static final tCredentials = Credentials(
+    email: 'jermelterzi@gmail.com',
+    password: 'Abc@12345',
+  );
+}
